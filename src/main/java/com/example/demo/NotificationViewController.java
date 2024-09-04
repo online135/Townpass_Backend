f23435631366e1ef0223de9362430a1fdeeffcd4
DESKTOP-4MPHLE6\User <b97b01067@gmail.com>
2024-09-04 23:03:19 +0800

    刪掉用不到的東西

--- src/main/java/com/example/demo/NotificationViewController.java
+++ src/main/java/com/example/demo/NotificationViewController.java
@@ -1,16 +1,10 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.client.RestTemplate;
-
 
 @Controller
 public class NotificationViewController {
-
-    @Autowired
-    private RestTemplate restTemplate;
 
     @GetMapping("/notifications")
     public void getNotifications() {
